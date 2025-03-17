27d6e8bd71b00fe0fc9e6e0ef5ef796e9e3293c3
Phil Butcher <phillip@upstream.tech>
2025-03-16 20:07:05 -0700

    simply parsing by albums logic

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/create_playlist")
+def create_playlist():
+    ...
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
